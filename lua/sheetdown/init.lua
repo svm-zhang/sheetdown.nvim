1b56ea1917d3596fd8ec8e592ea948fe1038f38b
simo <3883886+svm-zhang@users.noreply.github.com>
2026-03-25 10:15:20 -0700

    Added initial plugin entrypoint.

---
+++ lua/sheetdown/init.lua
@@ -0,0 +1,18 @@
+local config = require("sheetdown.config")
+local commands = require("sheetdown.commands")
+
+local M = {}
+
+---Configure sheetdown and return the effective config.
+---@param opts? table
+---@return table
+function M.setup(opts)
+  return config.setup(opts)
+end
+
+---Run the public table-generation workflow.
+function M.table_from_file()
+  commands.run()
+end
+
+return M
